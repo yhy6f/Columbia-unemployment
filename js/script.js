ba6e908dcdf18a200626b56c423f5f99e9fe8a41
Jasmine Ye Han <jasmine.han@foxmail.com>
2016-04-12 10:58:31 -0500

    add files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,75 @@
-$(document).ready(function() {
-    console.log("Hello world.")
-});
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatDate = d3.time.format("%Y-%m-%d");
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { 
+    	return x(formatDate.parse(d.observation_date)); 
+    })
+    .y(function(d) { 
+    	return y(d.CLMUR); 
+    });
+
+var svg = d3.select(".chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data.tsv", type, function(error, data) {
+  if (error) throw error;
+  console.log(data)
+  x.domain(d3.extent(data, function(d) { 
+      return d.observation_date; 
+}));
+
+  y.domain(d3.extent(data, function(d) { 
+      return d.CLMUR; 
+}));
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Unemployment rate");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+ });
+
+function type(d) {
+  d.observation_date = formatDate.parse(d.observation_date);
+  d.CLMUR = +d.CLMUR;
+  return d;
+}
+
 
 
 
